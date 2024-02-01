60188264203b27be623014b8935dae53de0c34cd
Гера Мертинс <geramertins@mail.ru>
2024-02-01 14:11:15 +0300

    Создан контроллер для работы с данными по заявкам.

---
+++ Controllers/OrderContoller.cs
@@ -0,0 +1,18 @@
+﻿using SCRINTest.Context;
+using SCRINTest.Models.DB;
+
+namespace SCRINTest.Controllers
+{
+    public static class OrderContoller
+    {
+        public static void AddOrders(List<BuyingProduct> orders, List<ProductsByuingProduct> productsByuingProduct)
+        {
+            using var db = new ScrintestContext();
+
+            db.BuyingProducts.AddRange(orders);
+            db.ProductsByuingProducts.AddRange(productsByuingProduct);
+
+            db.SaveChanges();
+        }
+    }
+}
