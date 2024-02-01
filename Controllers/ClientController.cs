0ef32462c4e5c650e505f716e85d92532e5f72b3
Гера Мертинс <geramertins@mail.ru>
2024-02-01 14:10:32 +0300

    Создан Controller для работы с данными пользователя.

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using SCRINTest.Context;
+using SCRINTest.Models.DB;
+
+namespace SCRINTest.Controllers
+{
+    public class ClientController
+    {
+        public static int SearchOrAddClientAndGetId (Client client)
+        {
+            using var db = new ScrintestContext();
+
+            int id = db.Clients.Where(x => x.Email == client.Email).Select(x => x.Id).FirstOrDefault();
+
+            if (id == 0)
+            {
+                db.Clients.Add(client);
+                db.SaveChanges();
+                id = client.Id;
+            }
+
+            return id;
+        }
+    }
+}
