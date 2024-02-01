09ca143656e23f3d6b9650c488916747fcf7c9c8
Гера Мертинс <geramertins@mail.ru>
2024-02-01 14:10:52 +0300

    Создан контроллер для работы с данными по продуктам.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using SCRINTest.Context;
+using SCRINTest.Models.DB;
+
+namespace SCRINTest.Controllers
+{
+    public static class ProductController
+    {
+        public static int SearchOrAddProductAndGetId(Product product)
+        {
+            using var db = new ScrintestContext();
+
+            int id = db.Products.Where(x => x.Name == product.Name).Select(x => x.Id).FirstOrDefault();
+
+            if (id == 0)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                id = product.Id;
+            }
+
+            return id;
+        }
+    }
+}
